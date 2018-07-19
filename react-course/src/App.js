fab343dd1b245332740d26f6ddf8c3744a86bfcd
Armando Ortiz <aortiz@sciodev.com>
2018-07-19 17:29:14 -0500

    Added first version of the course

---
+++ react-course/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>I made a react app, jej</h1>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi I\'am a react app lololo'));
+  }
+}
+
+export default App;
